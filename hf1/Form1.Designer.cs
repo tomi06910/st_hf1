fe2d697671eb162a3a7c0afb8e6a2ba69a191f9c
BeatrixSzekeres <rixi1125@gmail.com>
2017-02-09 11:49:00 +0100

    Gomb felirat megváltoztatás

--- hf1/Form1.Designer.cs
+++ hf1/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Összeadás";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
